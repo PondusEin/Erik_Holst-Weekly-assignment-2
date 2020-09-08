65a851f4c9dfae4540f86757b90f3572ba1152f9
PondusEin <PondusEin@users.noreply.github.com>
2020-09-08 14:14:10 +0200

    Finished task 2

--- Testing myarray/Testing myarray.cpp
+++ Testing myarray/Testing myarray.cpp
@@ -9,24 +9,29 @@ int main()
 	int count{ 1 };
 	while (count < 101) {
 		std::cout << count << ' ';
-		++count;
+		count++;
 	}
 	std::cout << "done! This was while-loop!\n\n\n"<<std::endl;
 
 	std::cout << "1.2. Use a for-loop:\n" << std::endl;
 
-	for (int count{ 1 }; count < 101; ++count)
-		std::cout << count << ' ';
+	for (int count1{ 1 }; count1 < 101; count1++){
+		std::cout << count1 << ' ';
+	
+	}
 	std::cout << "done! This was for-loop!\n\n\n" << std::endl;
 
+
+
+	std::cout << "2. Write a program that writes number from 100 down to 1 to screen. Use for-loop\n"<< std::endl;
+
+
+	for (int count2{ 100 }; count2 > 0; count2--)
+	{
+		std::cout << count2 << '\n';
 	
-}
-/*
-void(task2)
-{
-	for (int count{ 100 }; count < 0; --count)
-		std::cout << count << ' ';
-	std::cout << "done! This was for-loop!\n\n\n";
+	}
+	std::cout << "Done! This was for-loop!\n\n\n";
 	
 	return 0;
-}*/
+}
