0b276a2172e6da4e67d9bd55b8ee17453754a03b
PondusEin <PondusEin@users.noreply.github.com>
2020-09-08 14:54:02 +0200

    Finished task 5

--- Testing myarray/Testing myarray.cpp
+++ Testing myarray/Testing myarray.cpp
@@ -32,6 +32,26 @@ int main()
 	
 	}
 	std::cout << "Done! This was for-loop!\n\n\n";
-	
+
+
+	std::cout << "3. TBA\n\n\n" << std::endl;
+
+	std::cout << "4. Make a program that writes 5 to 50 in steps of 5. Use a while loop.\n" << std::endl;
+	int count5{ 5 };
+	while (count5 <51)
+		{
+		std::cout << count5 << ' ';
+			count5 == 50;
+		count5 += 5;
+		}
+	std::cout << "done! this was while-loop!\n\n\n";
+
+	std::cout << "5. Make a program that writes 5 to 50 in steps of 5. Use a for loop.\n" << std::endl;
+	for (int count6{ 5 }; count6<=50;count6+=5)
+	{
+		std::cout << count6 << ' ';
+	}
+
+	std::cout << "Done! This was for-loop!\n\n\n";
 	return 0;
 }
